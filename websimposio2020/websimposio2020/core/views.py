14bf8ea55d32b77e5d087c3241ddaa39cd45daf1
Marisol Diaz <mol97mx@gmail.com>
2020-06-01 19:01:21 -0500

    se remueven logos de la umsnh y se agrega nuevo poster

---
+++ websimposio2020/websimposio2020/core/views.py
@@ -0,0 +1,151 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.urls import reverse
+from django.http import JsonResponse
+import stripe
+stripe.api_key = "sk_test_vkm9UX8dSGs3JNkDWf0OiBf700Hqs16iz4"
+# Create your views here.
+
+def home(request):
+    return render (request,"core/home.html")
+
+def informacion(request):
+    return render (request,"core/informacion.html")
+
+def inscripcion(request):
+    return render (request,"core/inscripcion.html")
+    
+def resumenes(request):
+    return render (request,"core/resumenes.html")
+
+def instructores(request):
+    return render (request,"core/instructores.html")
+
+def calendario(request):
+    return render (request,"core/calendario.html")
+
+def semblanzaGPA(request):
+    return render (request,"core/semblanzaGPA.html")
+
+def semblanzaCCM(request):
+    return render (request,"core/semblanzaCCM.html")
+
+def semblanzaVMHM(request):
+    return render (request,"core/semblanzaVMHM.html")
+
+def semblanzaOPV(request):
+    return render (request,"core/semblanzaOPV.html")
+
+def semblanzaNFL(request):
+    return render (request,"core/semblanzaNFL.html")
+
+def semblanzaMLL(request):
+    return render (request,"core/semblanzaMLL.html")
+
+def semblanzaPBL(request):
+    return render (request,"core/semblanzaPBL.html")
+
+def patrocinadores(request):
+    return render (request,"core/patrocinadores.html")
+
+def contacto(request):
+    return render (request,"core/contacto.html")
+
+def acerca(request):
+    return render (request,"core/acerca.html")
+
+def quienes(request):
+    return render (request,"core/quienes.html")
+
+def precios(request):
+    return render (request,"core/precios.html")
+
+def talleres(request):
+    return render (request,"core/talleres.html")
+
+def conferencias(request):
+    return render (request,"core/conferencias.html")
+
+def conferencistas(request):
+    return render (request,"core/conferencistas.html")
+
+def ponencias(request):
+    return render (request,"core/ponencias.html")
+
+def charge(request):
+    if request.method=='POST':
+        print('Data:', request.POST)
+        if request.POST['amount']=='evento':
+            amount=int(1375)
+            desc="Inscripción al simposio"
+        elif request.POST['amount']=='curso1':
+            amount=int(1375)
+            desc="Inscripción a curso Valuación de bienes distintos a la tierra"
+        elif request.POST['amount']=='curso2':
+            amount=int(1375)
+            desc="Inscripción a curso Valuación de inmuebles rústicos con presión urbana"
+        elif request.POST['amount']=='curso3':
+            amount=int(1375)
+            desc="Inscripción a curso Valuación económica de impacto ambiental por el Método de Valoración Contingente"
+        else:
+            amount=int(2500)
+            desc="Inscripción a cursos y simposio"
+        try:
+        # Use Stripe's library to make requests...
+            cliente=stripe.Customer.create(
+                email=request.POST['email'],
+                name=request.POST['name'],
+                description=request.POST['instituto'],
+                phone=reques.POST['telefono'],
+                source=request.POST['stripeToken']
+            )
+            cargo=stripe.Charge.create(
+                customer=cliente,
+                amount=amount*100,
+                currency="mxn",
+                description=desc,
+            )
+            return redirect(reverse ('success', args=[amount]))
+            pass
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            #print('Status is: %s' % e.http_status)
+            #print('Type is: %s' % e.error.type)
+            #print('Code is: %s' % e.error.code)
+            # param is '' in this case
+            #print('Param is: %s' % e.error.param)
+            #print('Message is: %s' % e.error.message)
+            return redirect(reverse ('failure', args=[e.error.message]))
+        except stripe.error.RateLimitError as e:
+            # Too many requests made to the API too quickly
+            return redirect(reverse ('failure', args=[e.error.message]))
+            pass
+        except stripe.error.InvalidRequestError as e:
+            # Invalid parameters were supplied to Stripe's API
+            return redirect(reverse ('failure', args=[e.error.message]))
+            pass
+        except stripe.error.AuthenticationError as e:
+            # Authentication with Stripe's API failed
+            # (maybe you changed API keys recently)
+            return redirect(reverse ('failure', args=[e.error.message]))
+            pass
+        except stripe.error.APIConnectionError as e:
+            # Network communication with Stripe failed
+            return redirect(reverse ('failure', args=[e.error.message]))
+            pass
+        except stripe.error.StripeError as e:
+            # Display a very generic error to the user, and maybe send
+            # yourself an email
+            return redirect(reverse ('failure', args=[e.error.message]))
+            pass
+        except Exception as e:
+            # Something else happened, completely unrelated to Stripe
+            return redirect(reverse ('failure', args=[e.error.message]))
+            pass
+
+def success(request, args):
+    amount=args
+    return render(request,"core/success.html",{'amount':amount})
+
+def failure(request, args):
+    mensaje=args
+    return render(request,"core/failure.html",{'mensaje':mensaje})
---
+++ websimposio2020/websimposio2020/websimposio2020/urls.py
@@ -0,0 +1,55 @@
+"""websimposio2020 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+#from django.conf.urls import include, url
+from core import views as core_views
+from Galeria import views as galeria_views
+from django.conf import settings
+
+urlpatterns = [
+    path('',core_views.home,name="Home"),
+    path('informacion/',core_views.informacion,name="Informacion"),
+    path('inscripcion/',core_views.inscripcion,name="Inscripcion"),
+    path('resumenes/',core_views.resumenes,name="Resumenes"),
+    path('instructores/',core_views.instructores,name="Instructores"),
+    path('conferencias/',core_views.conferencias,name="Conferencias"),
+    path('conferencistas/',core_views.conferencistas,name="Conferencistas"),
+    path('ponencias/',core_views.ponencias,name="Ponencias"),
+    path('semblanzaGPA/',core_views.semblanzaGPA,name="SemblanzaGPA"),
+    path('semblanzaCCM/',core_views.semblanzaCCM,name="SemblanzaCCM"),
+    path('semblanzaVMHM/',core_views.semblanzaVMHM,name="SemblanzaVMHM"),
+    path('semblanzaOPV/',core_views.semblanzaOPV,name="SemblanzaOPV"),
+    path('semblanzaNFL/',core_views.semblanzaNFL,name="SemblanzaNFL"),
+    path('semblanzaMLL/',core_views.semblanzaMLL,name="SemblanzaMLL"),
+    path('semblanzaPBL/',core_views.semblanzaPBL,name="SemblanzaPBL"),
+    path('patrocinadores/',core_views.patrocinadores,name="Patrocinadores"),
+    path('talleres/',core_views.talleres,name="Talleres"),
+    path('contacto/',core_views.contacto,name="Contacto"),
+    path('acerca/',core_views.acerca,name="Acerca"),
+    path('quienes/',core_views.quienes,name="Quienes"),
+    path('precios/',core_views.precios,name="Precios"),
+    path('charge/',core_views.charge,name="charge"),
+    path('success/<str:args>',core_views.success,name="success"),
+    path('failure/<str:args>',core_views.failure,name="failure"),
+    path('calendario/',core_views.calendario,name="Calendario"),
+    path('galeria/',galeria_views.galeria,name="Galeria"),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
