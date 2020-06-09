ba79a5fff0e5e66a620150cf6110f6e95ddfa085
Marisol Diaz <mol97mx@gmail.com>
2020-06-08 22:00:53 -0500

    agregamos los logos solicitados por el cliente, cambiamos el menu del simposio, ajustamos la paginga quienes somos

--- websimposio2020/core/views.py
+++ websimposio2020/core/views.py
@@ -71,76 +71,8 @@ def conferencistas(request):
 def ponencias(request):
     return render (request,"core/ponencias.html")
 
-def charge(request):
-    if request.method=='POST':
-        print('Data:', request.POST)
-        if request.POST['amount']=='evento':
-            amount=int(1375)
-            desc="Inscripción al simposio"
-        elif request.POST['amount']=='curso1':
-            amount=int(1375)
-            desc="Inscripción a curso Valuación de bienes distintos a la tierra"
-        elif request.POST['amount']=='curso2':
-            amount=int(1375)
-            desc="Inscripción a curso Valuación de inmuebles rústicos con presión urbana"
-        elif request.POST['amount']=='curso3':
-            amount=int(1375)
-            desc="Inscripción a curso Valuación económica de impacto ambiental por el Método de Valoración Contingente"
-        else:
-            amount=int(2500)
-            desc="Inscripción a cursos y simposio"
-        try:
-        # Use Stripe's library to make requests...
-            cliente=stripe.Customer.create(
-                email=request.POST['email'],
-                name=request.POST['name'],
-                description=request.POST['instituto'],
-                phone=reques.POST['telefono'],
-                source=request.POST['stripeToken']
-            )
-            cargo=stripe.Charge.create(
-                customer=cliente,
-                amount=amount*100,
-                currency="mxn",
-                description=desc,
-            )
-            return redirect(reverse ('success', args=[amount]))
-            pass
-        except stripe.error.CardError as e:
-            # Since it's a decline, stripe.error.CardError will be caught
-            #print('Status is: %s' % e.http_status)
-            #print('Type is: %s' % e.error.type)
-            #print('Code is: %s' % e.error.code)
-            # param is '' in this case
-            #print('Param is: %s' % e.error.param)
-            #print('Message is: %s' % e.error.message)
-            return redirect(reverse ('failure', args=[e.error.message]))
-        except stripe.error.RateLimitError as e:
-            # Too many requests made to the API too quickly
-            return redirect(reverse ('failure', args=[e.error.message]))
-            pass
-        except stripe.error.InvalidRequestError as e:
-            # Invalid parameters were supplied to Stripe's API
-            return redirect(reverse ('failure', args=[e.error.message]))
-            pass
-        except stripe.error.AuthenticationError as e:
-            # Authentication with Stripe's API failed
-            # (maybe you changed API keys recently)
-            return redirect(reverse ('failure', args=[e.error.message]))
-            pass
-        except stripe.error.APIConnectionError as e:
-            # Network communication with Stripe failed
-            return redirect(reverse ('failure', args=[e.error.message]))
-            pass
-        except stripe.error.StripeError as e:
-            # Display a very generic error to the user, and maybe send
-            # yourself an email
-            return redirect(reverse ('failure', args=[e.error.message]))
-            pass
-        except Exception as e:
-            # Something else happened, completely unrelated to Stripe
-            return redirect(reverse ('failure', args=[e.error.message]))
-            pass
+def simposio(request):
+    return render (request,"core/simposio.html")
 
 def success(request, args):
     amount=args
