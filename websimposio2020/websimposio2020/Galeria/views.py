b4168e6c8d510483421acf286d59a56fced144d8
Marisol Diaz <mol97mx@gmail.com>
2020-06-10 09:28:53 -0500

    correcciones al titulo de la ponencia

--- websimposio2020/websimposio2020/Galeria/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Galeria
-
-# Create your views here.
-
-def galeria(request):
-    imagenes=Galeria.objects.all()
-    return render(request,"Galeria/galeria.html",{'Galeria':imagenes})
