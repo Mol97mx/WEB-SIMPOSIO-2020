747adc1f66d5b92d381d21be0978ec96c7ae1a08
Marisol Diaz <mol97mx@gmail.com>
2020-07-16 19:28:38 -0500

    se ajustan las opciones de precio del formularo de registro

--- websimposio2020/registros/models.py
+++ websimposio2020/registros/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 Opciones_paquetes = [
-    ('Curso Valuacion de bienes distintos a la tierra', 'Curso Valuación de bienes distintos a la tierra $1,375.00 MXN'),
-    ('Curso Valuacion de inmuebles rusticos con presion urbana', 'Curso Valuación de inmuebles rústicos con presión urbana $1,375.00 MXN'),
-    ('Curso Valuacion economica de impacto ambiental por el Metodo de valoracion contingente', 'Curso Valuación económica de impacto ambiental por el Método de Valoración Contingente $1,375.00 MXN'),
-    ('Simposio y curso', 'Simposio y curso $2,500.00 MXN'),
+    ('Curso Valuacion de bienes distintos a la tierra', 'Curso Valuación de bienes distintos a la tierra $1,595.00 MXN'),
+    ('Curso Valuacion de inmuebles rusticos con presion urbana', 'Curso Valuación de inmuebles rústicos con presión urbana $1,595.00 MXN'),
+    ('Curso Valuacion economica de impacto ambiental por el Metodo de valoracion contingente', 'Curso Valuación económica de impacto ambiental por el Método de Valoración Contingente $1,595.00 MXN'),
+    ('Simposio y curso', 'Simposio y curso $2,900.00 MXN'),
 ]
 OPCIONES=[('pendiente','Pendiente de pago'),('pagado','Pago confirmado')]
 
