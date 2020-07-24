b7799f4fa70c619a111feead43b55b8df119e986
Marisol Diaz <mol97mx@gmail.com>
2020-07-24 18:50:56 -0500

    se añade informacion para el deposito en la pagina de registro, se añade la opcion de solo simposio

--- websimposio2020/registros/models.py
+++ websimposio2020/registros/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 Opciones_paquetes = [
+    ('Simposio', 'Asistencia al simposio $1,595.00 MXN'),
     ('Curso Valuacion de bienes distintos a la tierra', 'Curso Valuación de bienes distintos a la tierra $1,595.00 MXN'),
     ('Curso Valuacion de inmuebles rusticos con presion urbana', 'Curso Valuación de inmuebles rústicos con presión urbana $1,595.00 MXN'),
     ('Curso Valuacion economica de impacto ambiental por el Metodo de valoracion contingente', 'Curso Valuación económica de impacto ambiental por el Método de Valoración Contingente $1,595.00 MXN'),
