19dc7cb07bd3b8159691b08082f2fb57659f1233
Marisol Diaz <mol97mx@gmail.com>
2020-07-29 20:26:03 -0500

    Se agrega titulo de la institucion

--- websimposio2020/websimposio2020/settings.py
+++ websimposio2020/websimposio2020/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER="invafmigh@gmail.com"
+EMAIL_HOST_USER="invafmich@gmail.com"
 EMAIL_HOST_PASSWORD="2005Invaf2020"
 EMAIL_PORT=587
 import os
