5a60eeecbde8aad7fd31736cd2a998ae68f21725
Marisol Diaz <mol97mx@gmail.com>
2020-08-03 19:27:41 -0500

    se añaden opciones de curos con simposio

--- websimposio2020/registros/models.py
+++ websimposio2020/registros/models.py
@@ -6,7 +6,9 @@ Opciones_paquetes = [
     ('Curso Valuacion de bienes distintos a la tierra', 'Curso Valuación de bienes distintos a la tierra $1,595.00 MXN'),
     ('Curso Valuacion de inmuebles rusticos con presion urbana', 'Curso Valuación de inmuebles rústicos con presión urbana $1,595.00 MXN'),
     ('Curso Valuacion economica de impacto ambiental por el Metodo de valoracion contingente', 'Curso Valuación económica de impacto ambiental por el Método de Valoración Contingente $1,595.00 MXN'),
-    ('Simposio y curso', 'Simposio y curso $2,900.00 MXN'),
+    ('Simposio y curso Valuacion de bienes distintos a la tierra', 'Simposio y curso Valuación de bienes distintos a la tierra $2,900.00 MXN'),
+    ('Simposio y curso Valuacion de inmuebles rusticos con presion urbana', 'Simposio y curso Valuacion de inmuebles rusticos con presion urbana $2,900.00 MXN'),
+    ('Simposio y curso Valuacion economica de impacto ambiental por el Metodo de valoracion contingente', 'Simposio y curso Valuación económica de impacto ambiental por el Método de Valoración Contingente $2,900.00 MXN'),
 ]
 OPCIONES=[('pendiente','Pendiente de pago'),('pagado','Pago confirmado')]
 
